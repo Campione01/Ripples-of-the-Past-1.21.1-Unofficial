c87af6c75a6e83451778a4f8dfbc1e32e73aaffd
Campione01 <w15628806223@gmail.com>
2026-09-13 03:19:39 -0700

    Preserve frozen arrow and trident spawn orientation

---
+++ src/main/java/com/github/standobyte/jojo/mixin/client/timestop/ProjectileTimeStopSpawnRotationMixin.java
@@ -0,0 +1,26 @@
+package com.github.standobyte.jojo.mixin.client.timestop;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.github.standobyte.jojo.subsystems.timestop.TimeStopState;
+
+import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.projectile.Projectile;
+
+@Mixin(Projectile.class)
+public class ProjectileTimeStopSpawnRotationMixin {
+
+	@Inject(method = "recreateFromPacket", at = @At("RETURN"))
+	private void jojo_ripples$preserveFrozenSpawnRotation(ClientboundAddEntityPacket packet, CallbackInfo ci) {
+		Projectile projectile = (Projectile) (Object) this;
+		if (projectile instanceof AbstractArrow && TimeStopState.shouldFreezeClientEntity(projectile)) {
+			// A following zero-motion packet must not treat the spawn rotation as uninitialized.
+			projectile.yRotO = projectile.getYRot();
+			projectile.xRotO = projectile.getXRot();
+		}
+	}
+}
